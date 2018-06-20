1541aa684b2c0df83a6048607fa41d91dc865b17
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2018-06-20 15:04:27 +0300

    add QQQ1.java

---
+++ src/aaa/bbb/ccc/QQQ1.java
@@ -0,0 +1,10 @@
+package aaa.bbb.ccc;
+
+/**
+ * *******************************
+ * Created by Irina.Petrovskaya on 6/20/2018.
+ * Project: nested_repos_detection
+ * *******************************
+ */
+public class QQQ1 {
+}
