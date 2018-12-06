3b5410afcc6846ef815256e882beb6b088af0225
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2018-12-06 19:53:15 +0300

    update my name

--- src/aaa/bbb/ccc/QQQ1.java
+++ src/aaa/bbb/ccc/QQQ1.java
@@ -2,7 +2,7 @@ package aaa.bbb.ccc;
 
 /**
  * *******************************
- * Created by Irina.Petrovskaya on 6/20/2018.
+ * Created by Irina Petrovskaya on 6/20/2018.
  * Project: nested_repos_detection
  * *******************************
  */
